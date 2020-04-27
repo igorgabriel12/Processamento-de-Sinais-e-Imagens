417eebe0bf07853a342f5598cecaa4e97dad616f
unknown <igor_gabryel@hotmail.com>
2020-04-27 20:31:32 -0300

    .

---
+++ Atividade1/Exercicio 1.py
@@ -0,0 +1,25 @@
+# Exercicio 01
+# Aluno: Igor Gabriel Pereira     
+#Cod.: 825.399
+
+# Buscar imagem no Github
+!wget "https://github.com/igorgabriel12/Processamento-de-Sinais-e-Imagens/blob/master/Atividade%20avaliativa%201/moedas.jpg"
+
+import cv2 as cv 
+import matplotlib.pyplot as plt
+
+img1 = cv.imread("objects.png");
+img1 = cv.cvtColor(img1,cv.COLOR_BGR2RGB)
+
+img2 = cv.cvtColor(img1,cv.COLOR_RGB2GRAY)
+ 
+ret, img2 = cv.threshold(img2,127,255,cv.THRESH_BINARY)
+ 
+contorno, ordem = cv.findContours(img2, cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
+
+for c in contorno: 
+  x,y,w,h = cv.boundingRect(c)
+  cv.rectangle(img1,(x,y),(x+w,y+h),(255,242,0),2)
+ 
+plt.figure(figsize=(8,8))
+plt.imshow(img1)
