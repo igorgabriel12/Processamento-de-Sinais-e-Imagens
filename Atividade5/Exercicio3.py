3218f320a8c6109f187b1553be3b646d83af9f5c
unknown <igor_gabryel@hotmail.com>
2020-05-19 23:40:34 -0300

    .

--- Atividade5/Exercicio3.py
+++ Atividade5/Exercicio3.py
@@ -2,9 +2,9 @@
 # Cod.: 825.399
 # Exercicio 3
 
-## OBS: Gostaria de ter tirado uma duvida a respeto deste exercicio sobre como reproduir o video normal, sem que fosse apenas os frames
+## OBS: Gostaria de ter tirado uma duvida a respeto deste exercicio sobre como reproduzir o video normal, sem que fosse apenas um frame abaixo do outro.
 
-!wget "https://vimeo.com/277147027" -O "teste.MP4"
+!wget "https://github.com/igorgabriel12/Processamento-de-Sinais-e-Imagens/blob/master/Atividade5/video.mp4" -O "video_original.mp4"
  
 import cv2
 from google.colab.patches import cv2_imshow
@@ -12,7 +12,7 @@ path = '/content/drive/My Drive/Disciplinas/Processamento de Sinais e Imagens/im
  
 face_cascade = cv2.CascadeClassifier(path + "cascades/haarcascade_frontalface_default.xml")
  
-cap = cv2.VideoCapture('video.mp4')
+cap = cv2.VideoCapture('video_original.mp4')
 
 while True: 
     _, img = cap.read() 
